86063dd55f2d291c547caef704360ee306467b27
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-09 15:18:55 +0530

    Create Pattern _15.java

---
+++ Pattern _15.java
@@ -0,0 +1,37 @@
+/*    * 
+      ** 
+      *** 
+      **** 
+      ***** 
+      **** 
+      *** 
+      ** 
+      *  */ 
+public class pattern_15
+
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                
+                System.out.print("*");
+                
+            }
+            System.out.println(" ");
+        }
+        for(i=4;i>0;i--)
+        {
+            for(j=0;j<i;j++)
+            {
+                
+                System.out.print("*");
+                
+            }
+            System.out.println(" ");
+        }
+    }
+}
