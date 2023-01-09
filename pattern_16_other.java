8ce820bda7f016c15c741840b9f83d8868903969
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-09 15:22:57 +0530

    Create pattern_16_other.java

---
+++ pattern_16_other.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class patt_16_other_way
+{
+    public static void main(String args[])
+    {
+        int n,i,j;
+        System.out.println("enter the value of n");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=n;i>0;i--)
+        {
+          for(j=0;j<i;j++)
+          {
+              System.out.print("*");
+          }
+          System.out.println(" ");
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+
+
+/* (for example if n=5)
+***** 
+**** 
+*** 
+** 
+* 
+*
+**
+***
+****
+*****
+  */
