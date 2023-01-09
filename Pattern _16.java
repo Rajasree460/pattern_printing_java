d879b5744f2a7f663e9eecdaf43e28e6bb028530
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-09 15:22:12 +0530

    Create Pattern _16.java

---
+++ Pattern _16.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class patt_16
+{
+    public static void main(String args[])
+    {
+        int n,i,j;
+        System.out.println("enter the value of n");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+          for(j=0;j<n-i;j++)
+          {
+              System.out.print("*");
+          }
+          System.out.println(" ");
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+
+
+/* (for example if n=5)
+***** 
+**** 
+*** 
+** 
+* 
+*
+**
+***
+****
+*****
+  */
