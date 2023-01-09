7e9f0da7bc08aa8bd8cbc16703425a7ab6b6b7a7
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-09 15:17:11 +0530

    Create Pattern _14.java

---
+++ Pattern _14.java
@@ -0,0 +1,23 @@
+/*    ***** 
+      **** 
+      *** 
+      ** 
+      *  */ 
+public class pattern_14
+
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        for(i=5;i>0;i--)
+        {
+            for(j=0;j<i;j++)
+            {
+                
+                System.out.print("*");
+                
+            }
+            System.out.println(" ");
+        }
+    }
+}
