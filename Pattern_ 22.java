0d80e51e8996a21da8c3dc0a89931a9bc0932591
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-12 12:18:28 +0530

    Create Pattern_ 22.java

---
+++ Pattern_ 22.java
@@ -0,0 +1,39 @@
+/* reverse pyramid */
+import java.util.*;
+public class patt_22
+{
+    public static void main(String args[])
+    {
+        int n,i,j,k;
+        System.out.println("enter the value of n");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=0;k<2*(n-i)-1;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+
+
+
+
+
+/*
+  
+(for example if n=3)
+  *****
+   ***
+    * 
+  
+ */
